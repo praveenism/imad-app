124ef11045048f50b3ddf7dc4659e80e7b1b65f7
praveenism <31377555+praveenism@users.noreply.github.com>
2018-03-02 15:16:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+title:'Article one by Praveen',
+heading:'Article 1',
+content:`
+    <p>
+        This is the content.
+    </p>
+    <p>
+        This is the content.
+
+    </p>`
+
+};
+function createTemplate(data){
+heading=data.heading;
+title=data.title;
+content=data.content;
+var htmlTemplate= `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href='/'>Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            Mar 2, 2018
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`
+;
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function (req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));});
+  res.send(createTemplate(articleOne));});
 
 app.get('/article-three',function (req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));});
