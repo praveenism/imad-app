c7594178a3a845de922774418d9fd58988f301b5
praveenism <31377555+praveenism@users.noreply.github.com>
2018-03-02 18:07:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function (req, res) {
+counter=counter+1;
+res.send(counter.toString());    
+});
+
 app.get('/:articleName',function (req,res){
   var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));});
@@ -103,11 +109,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var counter=0;
-app.get('/counter', function (req, res) {
-counter=counter+1;
-res.send(counter.toString());    
-});
 
 
 
