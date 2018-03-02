c13afa383b63e001d8a512c196a1f5806033880e
praveenism <31377555+praveenism@users.noreply.github.com>
2018-03-02 16:28:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,12 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="new value";
+var marginLeft=0;
 var img=document.getElementById('madi');
+function moveRight(){
+    marginLeft+=10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-  img.style.marginTop='100px'  ;
+    var interval=setInterval(moveRight,100);
 };
