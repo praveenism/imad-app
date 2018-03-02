52153d89e9e12df2e4183f43bf658de812b4d274
praveenism <31377555+praveenism@users.noreply.github.com>
2018-03-02 18:04:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 var counter=0;
 app.get('/counter', function (req, res) {
-counter+=1;
+counter=counter+1;
 res.send(counter.toString());    
 });
 
