3a409d794b31733060300daa89572405952f067c
praveenism <31377555+praveenism@users.noreply.github.com>
2018-03-06 09:42:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     
   var names=['name1','name2','name3','name4'];
-  var list='';
+  var list='<li>'+123+'</li>';
   for(var i=0;i<names.length;i++){
       list+='<li>' +names[i]+ '</li>';
   }
