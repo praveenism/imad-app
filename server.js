0aa0f546d652fc110972e4d0fdc43328670e924e
praveenism <31377555+praveenism@users.noreply.github.com>
2018-03-07 10:37:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config={
+      user:'thepraveenism',
+      database:'thepraveenism',
+      host:'db.imad.hasura-app.io',
+      port:'5432',
+      password:process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -86,6 +96,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool=new Pool(config);
+app.get('/test-dp', function(req,res){
+   pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringify(result));
+       }
+   } );  
+});
+
 var counter=0;
 app.get('/counter', function (req, res) {
 counter=counter+1;
@@ -124,3 +145,4 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+ 
